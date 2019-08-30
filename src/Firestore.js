53082e4296538bf203e5166790e541b34a5f1082
Charis Rafailidis <chrafailidis@gmail.com>
2019-08-30 11:00:32 +0300

    Update Firestore.js

--- src/Firestore.js
+++ src/Firestore.js
@@ -1,13 +1,15 @@
 import firebase from "firebase";
 
+// Replace the values below with your Firebase config
+
 var firebaseConfig = {
-    apiKey: "AIzaSyDapT1keNJAcKYd3Zs04i_fZH4jGdcmSzg",
-    authDomain: "narrative-51c3a.firebaseapp.com",
-    databaseURL: "https://narrative-51c3a.firebaseio.com",
-    projectId: "narrative-51c3a",
-    storageBucket: "narrative-51c3a.appspot.com",
-    messagingSenderId: "1093404319127",
-    appId: "1:1093404319127:web:bdc5592dad6a23a5"
+    apiKey: "yourAPIKeyHere",
+    authDomain: "yourauthDomainHere",
+    databaseURL: "yourdatabaseURLHere",
+    projectId: "yourprojectIdHere",
+    storageBucket: "yourstorageBucketHere",
+    messagingSenderId: "yourmessagingSenderIdHere",
+    appId: "yourappIdHere"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
